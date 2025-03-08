1ed482f0d4713c3eab56aad78fc6eb4eed3a2852
Kim <kimhogan10@gmail.com>
2025-03-08 13:55:25 -0500

    Update 002_dq_reputation_min_max.sql

--- stackoverflow/002_dq_reputation_min_max.sql
+++ stackoverflow/002_dq_reputation_min_max.sql
@@ -3,6 +3,8 @@ I then do some basic data quality checks to make sure I'm not making any poor as
 This also allows me to ensure I'm not duplicating the grain or expecting fields to have a value when they're null.
 
 Check: The range of values for reputation (to see if they can ever be negative or zero).
+
+Result: The min is 1 and the max is 1,357,603 - the reputation is never negative or zero, and there is a wide range of reputation values.
 */
 SELECT
   MIN(reputation) AS min_reputation,
