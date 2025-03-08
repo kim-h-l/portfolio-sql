f8c7828f7c986da9313fbc82312f0d2c48d60ef9
Kim <kimhogan10@gmail.com>
2025-03-08 13:57:18 -0500

    Update 001_user_data_exploration.sql

--- stackoverflow/001_user_data_exploration.sql
+++ stackoverflow/001_user_data_exploration.sql
@@ -2,6 +2,9 @@
 I like to get a good understanding of the data I'm working with by first looking at one example and expanding from there. 
 
 I went to stackoverflow.com and found the top user overall: https://stackoverflow.com/users/22656/jon-skeet
+
+Result: This user's account was created in 2008 (it's been 17 years since it was created)
+He has a total reputation of 1,357,603, including 17,030 up voes, 7775 down votes, and 2,177,751 views.
 */
 
 SELECT
