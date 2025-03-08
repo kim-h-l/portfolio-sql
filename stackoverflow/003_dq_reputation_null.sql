79bac837a7828e4d3135497348d9ce131d87db03
Kim <kimhogan10@gmail.com>
2025-03-08 13:53:53 -0500

    Update 003_dq_reputation_null.sql

--- stackoverflow/003_dq_reputation_null.sql
+++ stackoverflow/003_dq_reputation_null.sql
@@ -3,6 +3,8 @@ I then do some basic data quality checks to make sure I'm not making any poor as
 This also allows me to ensure I'm not duplicating the grain or expecting fields to have a value when they're null.
 
 Check: Can reputation ever be null?
+
+Result: The result is 0, which means the reputation is never null in the user table
 */
 
 SELECT
